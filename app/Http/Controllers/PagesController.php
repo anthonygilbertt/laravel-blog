877931f7b927f6a9733f84e6f9abe9faee6547ab
anthonygilbert <anthony.gilbert93@gmail.com>
2022-03-15 21:17:27 -0500

    init commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        // return 'INDEX';
+        // return view('test.article');
+        $title = 'Laravel Blog';
+        //return view('index', compact('title'));
+        return view('index')->with('fuck', $title);
+    }
+
+    public function about(){
+        $about = 'About';
+        return view('about')->with('shit', $about);
+    }
+
+    public function services(){
+            $data = array(
+                'title' => 'Services',
+                'services' => ['Programming', 'CI/CD']
+        );
+        return view('services')->with($data);
+    }
+    public function login(){
+    //     $data = array(
+    //         'title' => 'Services',
+    //         'services' => ['Programming', 'CI/CD']
+    // );
+    // return view('login')->with($data);
+        return view('index');
+    }
+
+
+    public function logout(){
+    //     $data = array(
+    //         'title' => 'Services',
+    //         'services' => ['Programming', 'CI/CD']
+    // );
+    // return view('login')->with($data);
+        return view('index');
+    }
+
+    public function signup(){
+    //     $data = array(
+    //         'title' => 'Services',
+    //         'services' => ['Programming', 'CI/CD']
+    // );
+    // return view('register')->with($data);
+    return view('signup');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/users/{id}/{name}', function ($id, $name){
+//     return 'This is user '. $name . ' with an ID of '. $id;
+// });
+
+// Route::get('/', function() {
+//     return "Hey";
+// });
+
+Route::get('/', [PagesController::class, 'index']);
+Route::get('/about', [PagesController::class, 'about']);
+Route::get('/services', [PagesController::class, 'services']);
+Route::get('/', [PagesController::class, 'logout']);
+Route::get('/', [PagesController::class, 'login']);
+Route::get('/signup', [PagesController::class, 'signup']);
+// Route::get('/', 'PagesController@index');
+// Route::get('/', function() {
+//     return "Hey";
+// });
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+// Route::get('/services', function () {
+//     return view('services');
+// });
