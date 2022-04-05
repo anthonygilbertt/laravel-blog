c62e683f5d336ce4016cbac775ca1598a594519a
anthonygilbert <anthony.gilbert93@gmail.com>
2022-04-04 21:22:13 -0500

    Re-generated the PostsController with CRUD functions

--- database/migrations/2022_03_25_050403_create_posts_table.php
+++ database/migrations/2022_03_25_050403_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->mediumText('body');
+            $table->mediumText('body'); // mediumtext is longer than a string
             $table->timestamps(); // created 2 columns (created at and updated at)
         });
     }
